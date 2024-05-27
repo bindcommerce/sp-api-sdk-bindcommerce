26b5b04c05c6e6aaafb0aadfb4ba0438c895272f
Angelo Galleja <angelo.galleja@intco.it>
2024-05-27 09:26:55 +0200

    Aggiunti url vendorcentral

--- src/AmazonPHP/SellingPartner/Marketplace.php
+++ src/AmazonPHP/SellingPartner/Marketplace.php
@@ -36,127 +36,148 @@ final class Marketplace
             'name'   => 'Canada',
             'id'     => 'A2EUQ1WTGCTBG2',
             'region' => Regions::NORTH_AMERICA,
-            'url'    => 'https://sellercentral.amazon.ca',
+            'sellercentral' => 'https://sellercentral.amazon.ca',
+            'vendorcentral' => 'https://vendorcentral.amazon.ca'
         ],
         'US' => [
             'name'   => 'United States of America',
             'id'     => 'ATVPDKIKX0DER',
             'region' => Regions::NORTH_AMERICA,
-            'url'    => 'https://sellercentral.amazon.com',
+            'sellercentral' => 'https://sellercentral.amazon.com',
+            'vendorcentral' => 'https://vendorcentral.amazon.com'
         ],
         'MX' => [
             'name'   => 'Mexico',
             'id'     => 'A1AM78C64UM0Y8',
             'region' => Regions::NORTH_AMERICA,
-            'url'    => 'https://sellercentral.amazon.com.mx',
+            'sellercentral' => 'https://sellercentral.amazon.com.mx',
+            'vendorcentral' => 'https://vendorcentral.amazon.com.mx'
         ],
         'BR' => [
             'name'   => 'Brazil',
             'id'     => 'A2Q3Y263D00KWC',
             'region' => Regions::NORTH_AMERICA,
-            'url'    => 'https://sellercentral.amazon.com.br',
+            'sellercentral' => 'https://sellercentral.amazon.com.br',
+            'vendorcentral' => 'https://vendorcentral.amazon.com.br'
         ],
         'ES' => [
             'name'   => 'Spain',
             'id'     => 'A1RKKUPIHCS9HS',
             'region' => Regions::EUROPE,
-            'url'    => 'https://sellercentral-europe.amazon.com',
+            'sellercentral' => 'https://sellercentral-europe.amazon.com',
+            'vendorcentral' => 'https://vendorcentral.amazon.es'
         ],
         'GB' => [
             'name'   => 'United Kingdom',
             'id'     => 'A1F83G8C2ARO7P',
             'region' => Regions::EUROPE,
-            'url'    => 'https://sellercentral-europe.amazon.com',
+            'sellercentral' => 'https://sellercentral-europe.amazon.com',
+            'vendorcentral' => 'https://vendorcentral.amazon.co.uk'
         ],
         'FR' => [
             'name'   => 'France',
             'id'     => 'A13V1IB3VIYZZH',
             'region' => Regions::EUROPE,
-            'url'    => 'https://sellercentral-europe.amazon.com',
+            'sellercentral' => 'https://sellercentral-europe.amazon.com',
+            'vendorcentral' => 'https://vendorcentral.amazon.fr'
         ],
         'BE' => [
             'name'   => 'Belgium',
             'id'     => 'AMEN7PMS3EDWL',
             'region' => Regions::EUROPE,
-            'url'    => 'https://sellercentral.amazon.com.be',
+            'sellercentral' => 'https://sellercentral.amazon.com.be',
+            'vendorcentral' => 'https://vendorcentral.amazon.com.be'
         ],
         'NL' => [
             'name'   => 'Netherlands',
             'id'     => 'A1805IZSGTT6HS',
             'region' => Regions::EUROPE,
-            'url'    => 'https://sellercentral.amazon.nl',
+            'sellercentral' => 'https://sellercentral.amazon.nl',
+            'vendorcentral' => 'https://vendorcentral.amazon.nl'
         ],
         'DE' => [
             'name'   => 'Germany',
             'id'     => 'A1PA6795UKMFR9',
             'region' => Regions::EUROPE,
-            'url'    => 'https://sellercentral-europe.amazon.com',
+            'sellercentral' => 'https://sellercentral-europe.amazon.com',
+            'vendorcentral' => 'https://vendorcentral.amazon.de'
         ],
         'IT' => [
             'name'   => 'Italy',
             'id'     => 'APJ6JRA9NG5V4',
             'region' => Regions::EUROPE,
-            'url'    => 'https://sellercentral-europe.amazon.com',
+            'sellercentral' => 'https://sellercentral-europe.amazon.com',
+            'vendorcentral' => 'https://vendorcentral.amazon.it'
         ],
         'SE' => [
             'name'   => 'Sweden',
             'id'     => 'A2NODRKZP88ZB9',
             'region' => Regions::EUROPE,
-            'url'    => 'https://sellercentral.amazon.se',
+            'sellercentral' => 'https://sellercentral.amazon.se',
+            'vendorcentral' => 'https://vendorcentral.amazon.se'
         ],
         'PL' => [
             'name'   => 'Poland',
             'id'     => 'A1C3SOZRARQ6R3',
             'region' => Regions::EUROPE,
-            'url'    => 'https://sellercentral.amazon.pl',
+            'sellercentral' => 'https://sellercentral.amazon.pl',
+            'vendorcentral' => 'https://vendorcentral.amazon.pl'
         ],
         'EG' => [
             'name'   => 'Egypt',
             'id'     => 'ARBP9OOSHTCHU',
             'region' => Regions::EUROPE,
-            'url'    => 'https://sellercentral.amazon.eg',
+            'sellercentral' => 'https://sellercentral.amazon.eg',
+            'vendorcentral' => 'https://vendorcentral.amazon.me'
         ],
         'TR' => [
             'name'   => 'Turkey',
             'id'     => 'A33AVAJ2PDY3EV',
             'region' => Regions::EUROPE,
-            'url'    => 'https://sellercentral.amazon.com.tr',
+            'sellercentral' => 'https://sellercentral.amazon.com.tr',
+            'vendorcentral' => 'https://vendorcentral.amazon.tr'
         ],
         'SA' => [
             'name'   => 'Saudi Arabia',
             'id'     => 'A17E79C6D8DWNP',
             'region' => Regions::EUROPE,
-            'url'    => 'https://sellercentral.amazon.sa',
+            'sellercentral' => 'https://sellercentral.amazon.sa',
+            'vendorcentral' => 'https://vendorcentral.amazon.me'
         ],
         'AE' => [
             'name'   => 'United Arab Emirates',
             'id'     => 'A2VIGQ35RCS4UG',
             'region' => Regions::EUROPE,
-            'url'    => 'https://sellercentral.amazon.ae',
+            'sellercentral' => 'https://sellercentral.amazon.ae',
+            'vendorcentral' => 'https://vendorcentral.amazon.me'
         ],
         'IN' => [
             'name'   => 'India',
             'id'     => 'A21TJRUUN4KGV',
             'region' => Regions::EUROPE,
-            'url'    => 'https://sellercentral.amazon.in',
+            'sellercentral' => 'https://sellercentral.amazon.in',
+            'vendorcentral' => 'https://www.vendorcentral.in'
         ],
         'SG' => [
             'name'   => 'Singapore',
             'id'     => 'A19VAU5U5O7RUS',
             'region' => Regions::FAR_EAST,
-            'url'    => 'https://sellercentral.amazon.sg',
+            'sellercentral' => 'https://sellercentral.amazon.sg',
+            'vendorcentral' => 'https://vendorcentral.amazon.com.sg'
         ],
         'AU' => [
             'name'   => 'Australia',
             'id'     => 'A39IBJ37TRP1C6',
             'region' => Regions::FAR_EAST,
-            'url'    => 'https://sellercentral.amazon.com.au',
+            'sellercentral' => 'https://sellercentral.amazon.com.au',
+            'vendorcentral' => 'https://vendorcentral.amazon.com.au'
         ],
         'JP' => [
             'name'   => 'Japan',
             'id'     => 'A1VC38T7YXB528',
             'region' => Regions::FAR_EAST,
-            'url'    => 'https://sellercentral.amazon.co.jp',
+            'sellercentral' => 'https://sellercentral.amazon.co.jp',
+            'vendorcentral' => 'https://vendorcentral.amazon.co.jp'
         ],
     ];
 
@@ -169,14 +190,17 @@ final class Marketplace
     private string $region;
 
     private string $sellerCentralUrl;
+    
+    private string $vendorCentralUrl;
 
-    private function __construct(string $name, string $id, string $countryCode, string $region, string $sellerCentralUrl)
+    private function __construct(string $name, string $id, string $countryCode, string $region, string $sellerCentralUrl, string $vendorCentralUrl)
     {
         $this->name = $name;
         $this->id = $id;
         $this->countryCode = $countryCode;
         $this->region = $region;
         $this->sellerCentralUrl = $sellerCentralUrl;
+        $this->vendorCentralUrl = $vendorCentralUrl;
     }
 
     /**
@@ -200,7 +224,7 @@ final class Marketplace
     {
         foreach (self::$countryMap as $countryCode => $item) {
             if (\strtoupper($marketplaceId) === $item['id']) {
-                return new self($item['name'], $item['id'], $countryCode, $item['region'], $item['url']);
+                return new self($item['name'], $item['id'], $countryCode, $item['region'], $item['sellercentral'], $item['vendorcentral']);
             }
         }
 
@@ -215,7 +239,7 @@ final class Marketplace
         $marketplaces = [];
 
         foreach (self::$countryMap as $countryCode => $item) {
-            $marketplaces[] = new self($item['name'], $item['id'], $countryCode, $item['region'], $item['url']);
+            $marketplaces[] = new self($item['name'], $item['id'], $countryCode, $item['region'], $item['sellercentral'], $item['vendorcentral']);
         }
 
         return $marketplaces;
@@ -229,7 +253,7 @@ final class Marketplace
 
         $map = self::$countryMap[$country];
 
-        return new self($map['name'], $map['id'], $country, $map['region'], $map['url']);
+        return new self($map['name'], $map['id'], $country, $map['region'], $map['sellercentral'], $map['vendorcentral']);
     }
 
     public function name() : string
@@ -255,5 +279,10 @@ final class Marketplace
     public function sellerCentralUrl() : string
     {
         return $this->sellerCentralUrl;
+    }
+
+    public function vendorCentralUrl() : string
+    {
+        return $this->vendorCentralUrl;
     }
 }
