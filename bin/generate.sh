672ecbcf14d34c72744a2814d0b0b84869c8c2c7
Angelo Galleja <angelo.galleja@intco.it>
2026-03-25 07:15:12 +0100

    OrdersV2026

--- bin/generate.sh
+++ bin/generate.sh
@@ -320,3 +320,11 @@ docker run --user "$(id -u)":"$(id -g)" --rm -v "${PWD}:/sp-api" openapitools/op
     --language-specific-primitives \\DateTimeInterface \
     --type-mappings date=\\DateTimeInterface,Date=\\DateTimeInterface,DateTime=\\DateTimeInterface
 
+docker run --user "$(id -u)":"$(id -g)" --rm -v "${PWD}:/sp-api" openapitools/openapi-generator-cli generate \
+    -i https://raw.githubusercontent.com/amzn/selling-partner-api-models/refs/heads/main/models/orders-api-model/orders_2026-01-01.json \
+    -c /sp-api/config/generator-ordersV2026.yaml \
+    --global-property models,apis,apiDocs=false,modelDocs=false,modelTests=false,apiTests=false,supportingFiles=false \
+    -o /sp-api \
+    --language-specific-primitives \\DateTimeInterface \
+    --type-mappings date=\\DateTimeInterface,Date=\\DateTimeInterface,DateTime=\\DateTimeInterface
+
