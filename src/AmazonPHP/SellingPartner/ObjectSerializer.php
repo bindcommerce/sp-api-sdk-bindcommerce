f2d8dac47577a4f1a1aa0f5b16e475833bfefa32
Angelo Galleja <angelo.galleja@intco.it>
2026-05-15 11:08:18 +0200

    rimossa validazione dei valori null

--- src/AmazonPHP/SellingPartner/ObjectSerializer.php
+++ src/AmazonPHP/SellingPartner/ObjectSerializer.php
@@ -70,21 +70,21 @@ final class ObjectSerializer
                     $value = $data->{$getter}();
 
                     if ($value !== null && \is_object($value) && \method_exists($value, 'getAllowableEnumValues')) {
-                        $callable = [$openAPIType, 'getAllowableEnumValues'];
+                        // $callable = [$openAPIType, 'getAllowableEnumValues'];
 
-                        if (\is_callable($callable)) {
-                            /** array $callable */
-                            $allowedEnumTypes = $callable();
+                        // if (\is_callable($callable)) {
+                        //     /** array $callable */
+                        //     $allowedEnumTypes = $callable();
 
-                            if (
-                                !\in_array($value->toString(), $allowedEnumTypes, true) &&
-                                !\in_array(\ltrim((string) $openAPIType, '\\'), self::getBrokenModelDefinitions(), true)
-                            ) {
-                                $imploded = \implode("', '", $allowedEnumTypes);
+                        //     if (
+                        //         !\in_array($value->toString(), $allowedEnumTypes, true) &&
+                        //         !\in_array(\ltrim((string) $openAPIType, '\\'), self::getBrokenModelDefinitions(), true)
+                        //     ) {
+                        //         $imploded = \implode("', '", $allowedEnumTypes);
 
-                                throw new \InvalidArgumentException("Invalid value for enum '{$openAPIType}', must be one of: '{$imploded}'");
-                            }
-                        }
+                        //         throw new \InvalidArgumentException("Invalid value for enum '{$openAPIType}', must be one of: '{$imploded}'");
+                        //     }
+                        // }
 
                         $value = $value->toString();
                     }
@@ -359,16 +359,16 @@ final class ObjectSerializer
         }
 
         if (\method_exists($class, 'getAllowableEnumValues')) {
-            // Do not validate if class is one of amazon broken model definitions.
-            if (\in_array(\ltrim($class, '\\'), self::getBrokenModelDefinitions(), true)) {
-                return new $class($data);
-            }
+            // // Do not validate if class is one of amazon broken model definitions.
+            // if (\in_array(\ltrim($class, '\\'), self::getBrokenModelDefinitions(), true)) {
+            //     return new $class($data);
+            // }
 
-            if (!\in_array($data, $class::getAllowableEnumValues(), true)) {
-                $imploded = \implode("', '", $class::getAllowableEnumValues());
+            // if (!\in_array($data, $class::getAllowableEnumValues(), true)) {
+            //     $imploded = \implode("', '", $class::getAllowableEnumValues());
 
-                throw new \InvalidArgumentException("Invalid value for enum '{$class}', must be one of: '{$imploded}'");
-            }
+            //     throw new \InvalidArgumentException("Invalid value for enum '{$class}', must be one of: '{$imploded}'");
+            // }
 
             return new $class($data);
         }
@@ -394,16 +394,24 @@ final class ObjectSerializer
         foreach ($instance::openAPITypes() as $property => $type) {
             $propertySetter = $instance::setters()[$property];
 
-            if (!isset($propertySetter) || !isset($data->{$instance::attributeMap()[$property]})) {
+            if (!isset($propertySetter)) {
                 continue;
             }
 
-            if (isset($data->{$instance::attributeMap()[$property]})) {
-                $propertyValue = self::castEmptyStringToNull($data->{$instance::attributeMap()[$property]}, $type);
-                $propertyValue = self::filterEmptyCollectionElement($propertyValue, $type);
+            if (!isset($data->{$instance::attributeMap()[$property]})) {
+                $default = self::getDefaultForPrimitiveType($type);
 
-                $instance->{$propertySetter}(self::deserialize($configuration, $propertyValue, $type, null));
+                if ($default !== null) {
+                    $instance->{$propertySetter}($default);
+                }
+
+                continue;
             }
+
+            $propertyValue = self::castEmptyStringToNull($data->{$instance::attributeMap()[$property]}, $type);
+            $propertyValue = self::filterEmptyCollectionElement($propertyValue, $type);
+
+            $instance->{$propertySetter}(self::deserialize($configuration, $propertyValue, $type, null));
         }
 
         return $instance;
@@ -438,6 +446,21 @@ final class ObjectSerializer
             \ltrim(SellerFreightClass::class, '\\'),
             \ltrim(OrderItem::class, '\\'),
         ];
+    }
+
+    /**
+     * Returns a safe default value for primitive PHP types.
+     * This prevents TypeErrors when Amazon omits required primitive fields from API responses.
+     */
+    private static function getDefaultForPrimitiveType(string $type): mixed
+    {
+        return match ($type) {
+            'bool', 'boolean' => false,
+            'int', 'integer' => 0,
+            'float', 'double', 'number' => 0.0,
+            'string' => '',
+            default => null,
+        };
     }
 
     /**
